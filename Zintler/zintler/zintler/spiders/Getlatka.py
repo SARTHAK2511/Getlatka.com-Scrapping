618925e123c7c1f3b708490889aa7310dd0be892
SaRTaK_B <90795608+SARTHAK2511@users.noreply.github.com>
2022-10-21 22:49:41 +0530

    Update Getlatka.py

--- Zintler/zintler/zintler/spiders/Getlatka.py
+++ Zintler/zintler/zintler/spiders/Getlatka.py
@@ -1,6 +1,7 @@
+
 import scrapy
 
-class ExampleSpider(scrapy.Spider):
+class Getlatkaspider(scrapy.Spider):
     name = 'getlatka.com'
     allowed_domains = ['getlatka.com']
     start_urls = ['https://getlatka.com/saas-companies']
@@ -18,24 +19,44 @@ class ExampleSpider(scrapy.Spider):
         Industry=[]
         asof=[]
         table = response.css("[class*='data-table_table'] tr")
-        tr=table[2]
-        i= 0
-        for tr in table:
+
+        for i in range(1,len(table)):
+            tr=table[i]
+            a=tr.xpath(f'td[3]/p/text()').extract()
+            b=tr.xpath(f'td[4]/p/text()').extract()
+            c=tr.xpath(f'td[5]/p/text()').extract()
+            d=tr.xpath(f'td[6]/span/text()').extract()
+            if(len(a)!=0):
+                fundings.append(a[1] )
+            else:
+                fundings.append(None)
+            if(len(b)!=0):
+                revenues.append(b[1] )
+            else:
+                revenues.append(None)
+            if(len(c)!=0):
+                valuation.append(c[1])
+            else:
+                valuation.append(None)
+            if(len(d)!=0):
+                cashFlow.append(d[1])
+            else:
+                cashFlow.append(None)
+            
             name.append(tr.css("[class*='cells_link'] ::text").get())
-            revenues.append(tr.xpath(f'//tr[{i}]/td[3]/p/text()').extract()[1])
-            fundings.append(tr.xpath(f'//tr[{i}]/td[4]/p/text()').extract()[1])
-            valuation.append(tr.xpath(f'//tr[{i}]/td[5]/p/text()').extract()[1])
-            cashFlow.append(tr.xpath(f'//tr[{i}]/td[6]/span/text()').extract()[1])
-            FirstName = tr.xpath(f'//tr[{i}]/td[7]/div/a/text()').extract()[0]
-            LastName = tr.xpath(f'//tr[{i}]/td[7]/div/a/text()').extract()[2]
+            
+       
+            
+            
+            FirstName = tr.xpath(f'td[7]/div/a/text()').extract()[0]
+            LastName = tr.xpath(f'td[7]/div/a/text()').extract()[2]
             Name=FirstName+" "+LastName
             Founder.append(Name)
-            Teamsize.append(tr.xpath(f'//tr[{i}]/td[8]/p/text()').extract()[1])
-            age.append(tr.xpath(f'//tr[{i}]/td[9]/p/text()').extract()[1])
-            location.append(tr.xpath(f'//tr[{i}]/td[10]/a/text()').extract()[1])
-            Industry.append(tr.xpath(f'//tr[{i}]/td[11]/a/text()').extract()[0])
-            asof.append(tr.xpath(f'//tr[{i}]/td[12]/p/text()').extract()[1])
-            i=i+1
-        
+            Teamsize.append(tr.xpath(f'td[8]/p/text()').extract()[1])
+            age.append(tr.xpath(f'td[9]/p/text()').extract()[1])
+            location.append(tr.xpath(f'td[10]/a/text()').extract()[1])
+            Industry.append(tr.xpath(f'td[11]/a/text()').extract()[0])
+            asof.append(tr.xpath(f'td[12]/p/text()').extract()[1])
+           
         yield {"Company Name": name,"Revenue":revenues,"Funding ":fundings,"Valuation":valuation,"CashFlow":cashFlow,"Founder Name":Founder,"TeamSize":Teamsize,"Age":age,"Location":location,"Industry":Industry,"Asof":asof}
         # scrapy crawl getlatka.com -o test.jsonscrapy crawl getlatka.com -o test.json
